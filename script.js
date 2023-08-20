fdb15b6eebc4a507be05fb333f77312d29be2b04
Evan Wilson <acechip09@gmail.com>
2023-08-19 23:23:25 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.val-btn').onclick = function(){
+  document.querySelector('.ticket-container').style.background = "#aeff00";
+  document.querySelector('.val-btn').style.display="none";
+  document.querySelector('.success-text').style.display="flex";
+}
